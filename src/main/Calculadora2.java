27eb78852979f61599b4989a006c6b0aa857a0c2
Carlos Pijanowski <carlos.pijanowski@gmail.com>
2021-10-08 11:44:51 -0300

    nova classe de testes para medir a cobertura

---
+++ src/main/Calculadora2.java
@@ -0,0 +1,15 @@
+package main;
+
+public class Calculadora2 {
+    public double soma (double... numeros){
+        double soma=0;
+
+        if(numeros.length>0) {
+            for (double num : numeros){
+                soma += num;
+            }
+        }
+
+        return soma;
+    }
+}
--- src/test/CalculadoraTest.java
+++ src/test/CalculadoraTest.java
@@ -1,14 +1,22 @@
 package test;
 
+import main.Calculadora;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class CalculadoraTest {
+    @Test
+    void seNaoInformarNumeroDeveRetornarZero() {
+        Calculadora calculadora = new Calculadora();
+        double soma = calculadora.soma();
+        Assertions.assertEquals(0,soma);
+    }
 
     @Test
     void devesomar() {
-
+        Calculadora calculadora = new Calculadora();
+        double soma = calculadora.soma(5, 5);
+        Assertions.assertEquals(10,soma);
     }
 
 }
